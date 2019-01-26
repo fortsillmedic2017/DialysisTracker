00129d901162047cb51002bb4c652be2859e98ff
Davis Hilton <fortsillmedic2017@gmail.com>
2019-01-25 22:57:01 -0500

    Project Start

---
+++ DialysisPatentTracker/DialysisPatentTracker/Controllers/LogInController.cs
@@ -0,0 +1,15 @@
+﻿// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DialysisPatentTracker.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DialysisPatentTracker/DialysisPatentTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DialysisPatentTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
